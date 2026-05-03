6bd727a193f4844b2c1f2dc9e815fc3328a0c981
Liu Feng <4282198@qq.com>
2026-05-04 00:06:55 +0800

    fix: add eslint node and jest globals config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,20 @@
+const globals = require('globals');
+
 module.exports = [
   {
     files: ['src/**/*.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'error',
+    },
+  },
+  {
+    files: ['src/**/*.test.js', 'src/**/__tests__/**/*.js'],
+    languageOptions: {
+      globals: globals.jest,
     },
   },
   {
